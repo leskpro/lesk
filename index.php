b6b7f4996d2fcec8679455a56151d9b63bd3e5e8
leskpro <q016737@bpo.mindbe.com.br>
2026-04-12 17:35:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$baseUrl = 'https://www.aaprodentemps.com/';
+$baseUrl = 'https://bitocountry.com/';
 
 function generateSegment($length) {
   $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
